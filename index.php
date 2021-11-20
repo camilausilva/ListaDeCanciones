e6558947cc31c91025e319bd3dd39860e01e4f1b
Camila Laureana Silva <87451942+camilausilva@users.noreply.github.com>
2021-11-20 20:21:03 -0300

    Contenido Inicial

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="/tpfinal/assets/css/style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+
+    <div class="shadow-lg p-3 mb-5" id="header">
+        <h1>MI LISTA DE CANCIONES</h1>
+    </div>
+
+    <table class="table table-dark" id="tabla">
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                <th>Artista/s</th>
+                <th>Duración</th>
+                <th>Colaboradores</th>
+                <th>Álbum</th>
+                <th>Track</th>
+                <th>Pais</th>
+                <th>Fecha de Salida</th>
+                <th>¿Es un Cover?</th>
+            </tr>
+        </thead>
+
+        <tbody>
+        <tr>
+            <td>Bohemian Rhapsody</td>
+            <td>Queen</td>
+            <td>3:45</td>
+            <td>Ninguno/a</td>
+            <td>A Night At The Opera</td>
+            <td>11</td>
+            <td>Pais</td>
+            <td>infinito</td>
+            <td>como?</td>
+        </tr>
+        <tr>
+            <td>Bohemian Rhapsody</td>
+            <td>Queen</td>
+            <td>3:45</td>
+            <td>Ninguno/a</td>
+            <td>A Night At The Opera</td>
+            <td>11</td>
+            <td>Pais</td>
+            <td>infinito</td>
+            <td>como?</td>
+        </tr>
+        <tr>
+            <td>Bohemian Rhapsody</td>
+            <td>Queen</td>
+            <td>3:45</td>
+            <td>Ninguno/a</td>
+            <td>A Night At The Opera</td>
+            <td>11</td>
+            <td>Pais</td>
+            <td>infinito</td>
+            <td>como?</td>
+        </tr>
+        </tbody>
+    </table>
+
+</body>
+</html>
