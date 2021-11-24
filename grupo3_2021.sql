18effc8e2d5bad82d41e9f603431542c06b1312e
Juani <87107074+VB6Enjoyer@users.noreply.github.com>
2021-11-23 22:24:16 -0300

    Query SQL para la DB actualizada
    Añadidas tablas "paises", "generos" y "artistas_canciones"

--- grupo3_2021.sql
+++ grupo3_2021.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-11-2021 a las 12:00:41
+-- Tiempo de generación: 24-11-2021 a las 02:23:20
 -- Versión del servidor: 10.4.21-MariaDB
 -- Versión de PHP: 8.0.12
 
@@ -45,7 +45,8 @@ INSERT INTO `albumes` (`idAlbumes`, `titulo`, `lanzamiento`) VALUES
 (5, 'Jar of Flies', 1994),
 (6, 'Above (Deluxe Edition)', 2013),
 (7, 'The Midnight Organ Fight', 2008),
-(8, 'Writer\'s Block', 2004);
+(8, 'Writer\'s Block', 2004),
+(9, 'A.D.N', 2010);
 
 -- --------------------------------------------------------
 
@@ -56,23 +57,26 @@ INSERT INTO `albumes` (`idAlbumes`, `titulo`, `lanzamiento`) VALUES
 CREATE TABLE `artistas` (
   `idArtistas` int(11) NOT NULL,
   `nombre` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
-  `nacionalidad` varchar(64) CHARACTER SET utf8mb4 NOT NULL
+  `pais` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `artistas`
 --
 
-INSERT INTO `artistas` (`idArtistas`, `nombre`, `nacionalidad`) VALUES
-(1, 'In Mourning', 'Suecia'),
-(2, 'Funkadelic', 'Estados Unidos'),
-(3, 'Pantera', 'Estados Unidos'),
-(4, 'Berri Txarrak', 'España'),
-(5, 'Alice in Chains', 'Estados Unidos'),
-(6, 'Mad Season', 'Estados Unidos'),
-(7, 'Frightened Rabbit', 'Reino Unido'),
-(8, 'Evergreen Terrace', 'Estados Unidos'),
-(11, 'Tim McIlrath', 'Estados Unidos');
+INSERT INTO `artistas` (`idArtistas`, `nombre`, `pais`) VALUES
+(1, 'In Mourning', 207),
+(2, 'Funkadelic', 75),
+(3, 'Pantera', 75),
+(4, 'Berri Txarrak', 73),
+(5, 'Alice in Chains', 75),
+(6, 'Mad Season', 75),
+(7, 'Frightened Rabbit', 180),
+(8, 'Evergreen Terrace', 75),
+(11, 'Tim McIlrath', 75),
+(12, 'Michael Sembello', 75),
+(13, 'Helker', 13),
+(14, 'Queen', 180);
 
 -- --------------------------------------------------------
 
@@ -97,7 +101,36 @@ INSERT INTO `artistas_albumes` (`idArtistas`, `idAlbumes`) VALUES
 (5, 5),
 (6, 6),
 (7, 7),
-(8, 8);
+(8, 8),
+(13, 9);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `artistas_canciones`
+--
+
+CREATE TABLE `artistas_canciones` (
+  `idArtistas` int(11) NOT NULL,
+  `idCanciones` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `artistas_canciones`
+--
+
+INSERT INTO `artistas_canciones` (`idArtistas`, `idCanciones`) VALUES
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5),
+(6, 6),
+(7, 7),
+(8, 8),
+(4, 9),
+(11, 9),
+(13, 10);
 
 -- --------------------------------------------------------
 
@@ -109,9 +142,9 @@ CREATE TABLE `canciones` (
   `idCanciones` int(11) NOT NULL,
   `titulo` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
   `duracion` varchar(6) CHARACTER SET utf8mb4 NOT NULL,
-  `genero` varchar(64) CHARACTER SET utf8mb4 NOT NULL,
+  `genero` int(11) NOT NULL,
   `cover` tinyint(1) NOT NULL,
-  `artistaOriginal` int(11) NOT NULL
+  `artistaOriginal` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
 --
@@ -119,15 +152,16 @@ CREATE TABLE `canciones` (
 --
 
 INSERT INTO `canciones` (`idCanciones`, `titulo`, `duracion`, `genero`, `cover`, `artistaOriginal`) VALUES
-(1, 'Colossus', '9:33', 'Death Metal Melódico Progresivo', 0, 0),
-(2, 'Maggot Brain', '10:18', 'Funk Rock', 0, 0),
-(3, 'Floods', '7:03', 'Groove Metal', 0, 0),
-(4, 'Gezur bat mila aldiz', '4:36', 'Hardcore Melódico', 0, 0),
-(5, 'Nutshell', '4:16', 'Grunge', 0, 0),
-(6, 'Slip Away', '5:39', 'Rock Alternativo', 0, 0),
-(7, 'My Backwards Walk', '3:29', 'Indie Rock', 0, 0),
-(8, 'Maniac', '3:13', 'Hardcore Melódico', 1, 0),
-(9, 'Denak ez du balio', '4:13', 'Hardcore Melódico', 0, 0);
+(1, 'Colossus', '9:33', 6, 0, NULL),
+(2, 'Maggot Brain', '10:18', 1, 0, NULL),
+(3, 'Floods', '7:03', 5, 0, NULL),
+(4, 'Gezur bat mila aldiz', '4:36', 7, 0, NULL),
+(5, 'Nutshell', '4:16', 2, 0, NULL),
+(6, 'Slip Away', '5:39', 2, 0, NULL),
+(7, 'My Backwards Walk', '3:29', 2, 0, NULL),
+(8, 'Maniac', '3:13', 6, 1, 12),
+(9, 'Denak ez du balio', '4:13', 7, 0, NULL),
+(10, 'The Show Must Go On', '4:30', 5, 1, 14);
 
 -- --------------------------------------------------------
 
@@ -154,7 +188,8 @@ INSERT INTO `canciones_albumes` (`idAlbumes`, `idCanciones`, `pista`) VALUES
 (6, 6, 14),
 (7, 7, 9),
 (8, 8, 1),
-(4, 9, 8);
+(4, 9, 8),
+(9, 10, 12);
 
 -- --------------------------------------------------------
 
@@ -181,7 +216,301 @@ INSERT INTO `canciones_usuarios` (`idUsuario`, `idCanciones`, `favorito`) VALUES
 (1, 6, 1),
 (1, 7, 1),
 (1, 8, 1),
-(1, 9, 1);
+(1, 9, 1),
+(1, 10, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `generos`
+--
+
+CREATE TABLE `generos` (
+  `idGeneros` int(11) NOT NULL,
+  `nombreGenero` varchar(64) COLLATE utf8mb4_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `generos`
+--
+
+INSERT INTO `generos` (`idGeneros`, `nombreGenero`) VALUES
+(1, 'Rock Clásico'),
+(2, 'Rock Moderno'),
+(3, 'Pop'),
+(4, 'Rap'),
+(5, 'Metal'),
+(6, 'Metal Extremo'),
+(7, 'Punk'),
+(8, 'Country'),
+(9, 'Electrónica'),
+(10, 'Jazz'),
+(11, 'Blues'),
+(12, 'Funk/Soul'),
+(13, 'Folk'),
+(14, 'Acústico'),
+(15, 'Reggae'),
+(16, 'Cumbia'),
+(17, 'Reggaeton'),
+(18, 'Otro');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `paises`
+--
+
+CREATE TABLE `paises` (
+  `id` int(11) NOT NULL,
+  `iso` char(2) DEFAULT NULL,
+  `nombrePais` varchar(80) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `paises`
+--
+
+INSERT INTO `paises` (`id`, `iso`, `nombrePais`) VALUES
+(1, 'AF', 'Afganistán'),
+(2, 'AX', 'Islas Gland'),
+(3, 'AL', 'Albania'),
+(4, 'DE', 'Alemania'),
+(5, 'AD', 'Andorra'),
+(6, 'AO', 'Angola'),
+(7, 'AI', 'Anguilla'),
+(8, 'AQ', 'Antártida'),
+(9, 'AG', 'Antigua y Barbuda'),
+(10, 'AN', 'Antillas Holandesas'),
+(11, 'SA', 'Arabia Saudí'),
+(12, 'DZ', 'Argelia'),
+(13, 'AR', 'Argentina'),
+(14, 'AM', 'Armenia'),
+(15, 'AW', 'Aruba'),
+(16, 'AU', 'Australia'),
+(17, 'AT', 'Austria'),
+(18, 'AZ', 'Azerbaiyán'),
+(19, 'BS', 'Bahamas'),
+(20, 'BH', 'Bahréin'),
+(21, 'BD', 'Bangladesh'),
+(22, 'BB', 'Barbados'),
+(23, 'BY', 'Bielorrusia'),
+(24, 'BE', 'Bélgica'),
+(25, 'BZ', 'Belice'),
+(26, 'BJ', 'Benin'),
+(27, 'BM', 'Bermudas'),
+(28, 'BT', 'Bhután'),
+(29, 'BO', 'Bolivia'),
+(30, 'BA', 'Bosnia y Herzegovina'),
+(31, 'BW', 'Botsuana'),
+(32, 'BV', 'Isla Bouvet'),
+(33, 'BR', 'Brasil'),
+(34, 'BN', 'Brunéi'),
+(35, 'BG', 'Bulgaria'),
+(36, 'BF', 'Burkina Faso'),
+(37, 'BI', 'Burundi'),
+(38, 'CV', 'Cabo Verde'),
+(39, 'KY', 'Islas Caimán'),
+(40, 'KH', 'Camboya'),
+(41, 'CM', 'Camerún'),
+(42, 'CA', 'Canadá'),
+(43, 'CF', 'República Centroafricana'),
+(44, 'TD', 'Chad'),
+(45, 'CZ', 'República Checa'),
+(46, 'CL', 'Chile'),
+(47, 'CN', 'China'),
+(48, 'CY', 'Chipre'),
+(49, 'CX', 'Isla de Navidad'),
+(50, 'VA', 'Ciudad del Vaticano'),
+(51, 'CC', 'Islas Cocos'),
+(52, 'CO', 'Colombia'),
+(53, 'KM', 'Comoras'),
+(54, 'CD', 'República Democrática del Congo'),
+(55, 'CG', 'Congo'),
+(56, 'CK', 'Islas Cook'),
+(57, 'KP', 'Corea del Norte'),
+(58, 'KR', 'Corea del Sur'),
+(59, 'CI', 'Costa de Marfil'),
+(60, 'CR', 'Costa Rica'),
+(61, 'HR', 'Croacia'),
+(62, 'CU', 'Cuba'),
+(63, 'DK', 'Dinamarca'),
+(64, 'DM', 'Dominica'),
+(65, 'DO', 'República Dominicana'),
+(66, 'EC', 'Ecuador'),
+(67, 'EG', 'Egipto'),
+(68, 'SV', 'El Salvador'),
+(69, 'AE', 'Emiratos Árabes Unidos'),
+(70, 'ER', 'Eritrea'),
+(71, 'SK', 'Eslovaquia'),
+(72, 'SI', 'Eslovenia'),
+(73, 'ES', 'España'),
+(74, 'UM', 'Islas ultramarinas de Estados Unidos'),
+(75, 'US', 'Estados Unidos'),
+(76, 'EE', 'Estonia'),
+(77, 'ET', 'Etiopía'),
+(78, 'FO', 'Islas Feroe'),
+(79, 'PH', 'Filipinas'),
+(80, 'FI', 'Finlandia'),
+(81, 'FJ', 'Fiyi'),
+(82, 'FR', 'Francia'),
+(83, 'GA', 'Gabón'),
+(84, 'GM', 'Gambia'),
+(85, 'GE', 'Georgia'),
+(86, 'GS', 'Islas Georgias del Sur y Sandwich del Sur'),
+(87, 'GH', 'Ghana'),
+(88, 'GI', 'Gibraltar'),
+(89, 'GD', 'Granada'),
+(90, 'GR', 'Grecia'),
+(91, 'GL', 'Groenlandia'),
+(92, 'GP', 'Guadalupe'),
+(93, 'GU', 'Guam'),
+(94, 'GT', 'Guatemala'),
+(95, 'GF', 'Guayana Francesa'),
+(96, 'GN', 'Guinea'),
+(97, 'GQ', 'Guinea Ecuatorial'),
+(98, 'GW', 'Guinea-Bissau'),
+(99, 'GY', 'Guyana'),
+(100, 'HT', 'Haití'),
+(101, 'HM', 'Islas Heard y McDonald'),
+(102, 'HN', 'Honduras'),
+(103, 'HK', 'Hong Kong'),
+(104, 'HU', 'Hungría'),
+(105, 'IN', 'India'),
+(106, 'ID', 'Indonesia'),
+(107, 'IR', 'Irán'),
+(108, 'IQ', 'Iraq'),
+(109, 'IE', 'Irlanda'),
+(110, 'IS', 'Islandia'),
+(111, 'IL', 'Israel'),
+(112, 'IT', 'Italia'),
+(113, 'JM', 'Jamaica'),
+(114, 'JP', 'Japón'),
+(115, 'JO', 'Jordania'),
+(116, 'KZ', 'Kazajstán'),
+(117, 'KE', 'Kenia'),
+(118, 'KG', 'Kirguistán'),
+(119, 'KI', 'Kiribati'),
+(120, 'KW', 'Kuwait'),
+(121, 'LA', 'Laos'),
+(122, 'LS', 'Lesotho'),
+(123, 'LV', 'Letonia'),
+(124, 'LB', 'Líbano'),
+(125, 'LR', 'Liberia'),
+(126, 'LY', 'Libia'),
+(127, 'LI', 'Liechtenstein'),
+(128, 'LT', 'Lituania'),
+(129, 'LU', 'Luxemburgo'),
+(130, 'MO', 'Macao'),
+(131, 'MK', 'ARY Macedonia'),
+(132, 'MG', 'Madagascar'),
+(133, 'MY', 'Malasia'),
+(134, 'MW', 'Malawi'),
+(135, 'MV', 'Maldivas'),
+(136, 'ML', 'Malí'),
+(137, 'MT', 'Malta'),
+(138, 'FK', 'Islas Malvinas'),
+(139, 'MP', 'Islas Marianas del Norte'),
+(140, 'MA', 'Marruecos'),
+(141, 'MH', 'Islas Marshall'),
+(142, 'MQ', 'Martinica'),
+(143, 'MU', 'Mauricio'),
+(144, 'MR', 'Mauritania'),
+(145, 'YT', 'Mayotte'),
+(146, 'MX', 'México'),
+(147, 'FM', 'Micronesia'),
+(148, 'MD', 'Moldavia'),
+(149, 'MC', 'Mónaco'),
+(150, 'MN', 'Mongolia'),
+(151, 'MS', 'Montserrat'),
+(152, 'MZ', 'Mozambique'),
+(153, 'MM', 'Myanmar'),
+(154, 'NA', 'Namibia'),
+(155, 'NR', 'Nauru'),
+(156, 'NP', 'Nepal'),
+(157, 'NI', 'Nicaragua'),
+(158, 'NE', 'Níger'),
+(159, 'NG', 'Nigeria'),
+(160, 'NU', 'Niue'),
+(161, 'NF', 'Isla Norfolk'),
+(162, 'NO', 'Noruega'),
+(163, 'NC', 'Nueva Caledonia'),
+(164, 'NZ', 'Nueva Zelanda'),
+(165, 'OM', 'Omán'),
+(166, 'NL', 'Países Bajos'),
+(167, 'PK', 'Pakistán'),
+(168, 'PW', 'Palau'),
+(169, 'PS', 'Palestina'),
+(170, 'PA', 'Panamá'),
+(171, 'PG', 'Papúa Nueva Guinea'),
+(172, 'PY', 'Paraguay'),
+(173, 'PE', 'Perú'),
+(174, 'PN', 'Islas Pitcairn'),
+(175, 'PF', 'Polinesia Francesa'),
+(176, 'PL', 'Polonia'),
+(177, 'PT', 'Portugal'),
+(178, 'PR', 'Puerto Rico'),
+(179, 'QA', 'Qatar'),
+(180, 'GB', 'Reino Unido'),
+(181, 'RE', 'Reunión'),
+(182, 'RW', 'Ruanda'),
+(183, 'RO', 'Rumania'),
+(184, 'RU', 'Rusia'),
+(185, 'EH', 'Sahara Occidental'),
+(186, 'SB', 'Islas Salomón'),
+(187, 'WS', 'Samoa'),
+(188, 'AS', 'Samoa Americana'),
+(189, 'KN', 'San Cristóbal y Nevis'),
+(190, 'SM', 'San Marino'),
+(191, 'PM', 'San Pedro y Miquelón'),
+(192, 'VC', 'San Vicente y las Granadinas'),
+(193, 'SH', 'Santa Helena'),
+(194, 'LC', 'Santa Lucía'),
+(195, 'ST', 'Santo Tomé y Príncipe'),
+(196, 'SN', 'Senegal'),
+(197, 'CS', 'Serbia y Montenegro'),
+(198, 'SC', 'Seychelles'),
+(199, 'SL', 'Sierra Leona'),
+(200, 'SG', 'Singapur'),
+(201, 'SY', 'Siria'),
+(202, 'SO', 'Somalia'),
+(203, 'LK', 'Sri Lanka'),
+(204, 'SZ', 'Suazilandia'),
+(205, 'ZA', 'Sudáfrica'),
+(206, 'SD', 'Sudán'),
+(207, 'SE', 'Suecia'),
+(208, 'CH', 'Suiza'),
+(209, 'SR', 'Surinam'),
+(210, 'SJ', 'Svalbard y Jan Mayen'),
+(211, 'TH', 'Tailandia'),
+(212, 'TW', 'Taiwán'),
+(213, 'TZ', 'Tanzania'),
+(214, 'TJ', 'Tayikistán'),
+(215, 'IO', 'Territorio Británico del Océano Índico'),
+(216, 'TF', 'Territorios Australes Franceses'),
+(217, 'TL', 'Timor Oriental'),
+(218, 'TG', 'Togo'),
+(219, 'TK', 'Tokelau'),
+(220, 'TO', 'Tonga'),
+(221, 'TT', 'Trinidad y Tobago'),
+(222, 'TN', 'Túnez'),
+(223, 'TC', 'Islas Turcas y Caicos'),
+(224, 'TM', 'Turkmenistán'),
+(225, 'TR', 'Turquía'),
+(226, 'TV', 'Tuvalu'),
+(227, 'UA', 'Ucrania'),
+(228, 'UG', 'Uganda'),
+(229, 'UY', 'Uruguay'),
+(230, 'UZ', 'Uzbekistán'),
+(231, 'VU', 'Vanuatu'),
+(232, 'VE', 'Venezuela'),
+(233, 'VN', 'Vietnam'),
+(234, 'VG', 'Islas Vírgenes Británicas'),
+(235, 'VI', 'Islas Vírgenes de los Estados Unidos'),
+(236, 'WF', 'Wallis y Futuna'),
+(237, 'YE', 'Yemen'),
+(238, 'DJ', 'Yibuti'),
+(239, 'ZM', 'Zambia'),
+(240, 'ZW', 'Zimbabue');
 
 -- --------------------------------------------------------
 
@@ -216,7 +545,8 @@ ALTER TABLE `albumes`
 -- Indices de la tabla `artistas`
 --
 ALTER TABLE `artistas`
-  ADD PRIMARY KEY (`idArtistas`);
+  ADD PRIMARY KEY (`idArtistas`),
+  ADD KEY `FK_ArtistasPaises` (`pais`);
 
 --
 -- Indices de la tabla `artistas_albumes`
@@ -226,11 +556,19 @@ ALTER TABLE `artistas_albumes`
   ADD KEY `FK_AlbumesArtistas` (`idAlbumes`);
 
 --
+-- Indices de la tabla `artistas_canciones`
+--
+ALTER TABLE `artistas_canciones`
+  ADD KEY `FK_ArtistasCanciones` (`idArtistas`),
+  ADD KEY `FK_CancionesArtistas` (`idCanciones`);
+
+--
 -- Indices de la tabla `canciones`
 --
 ALTER TABLE `canciones`
   ADD PRIMARY KEY (`idCanciones`),
-  ADD KEY `artistaOriginal` (`artistaOriginal`);
+  ADD KEY `artistaOriginal` (`artistaOriginal`),
+  ADD KEY `FK_Generos` (`genero`);
 
 --
 -- Indices de la tabla `canciones_albumes`
@@ -247,6 +585,18 @@ ALTER TABLE `canciones_usuarios`
   ADD KEY `FK_CancionesUsuarios` (`idCanciones`) USING BTREE;
 
 --
+-- Indices de la tabla `generos`
+--
+ALTER TABLE `generos`
+  ADD PRIMARY KEY (`idGeneros`);
+
+--
+-- Indices de la tabla `paises`
+--
+ALTER TABLE `paises`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
@@ -260,19 +610,31 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `albumes`
 --
 ALTER TABLE `albumes`
-  MODIFY `idAlbumes` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `idAlbumes` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `artistas`
 --
 ALTER TABLE `artistas`
-  MODIFY `idArtistas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `idArtistas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT de la tabla `canciones`
 --
 ALTER TABLE `canciones`
-  MODIFY `idCanciones` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `idCanciones` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de la tabla `generos`
+--
+ALTER TABLE `generos`
+  MODIFY `idGeneros` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT de la tabla `paises`
+--
+ALTER TABLE `paises`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=241;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
@@ -285,11 +647,31 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- Filtros para la tabla `artistas`
+--
+ALTER TABLE `artistas`
+  ADD CONSTRAINT `FK_ArtistasPaises` FOREIGN KEY (`pais`) REFERENCES `paises` (`id`);
+
+--
 -- Filtros para la tabla `artistas_albumes`
 --
 ALTER TABLE `artistas_albumes`
   ADD CONSTRAINT `FK_AlbumesArtistas` FOREIGN KEY (`idAlbumes`) REFERENCES `albumes` (`idAlbumes`),
   ADD CONSTRAINT `FK_ArtistasAlbumes` FOREIGN KEY (`idArtistas`) REFERENCES `artistas` (`idArtistas`);
+
+--
+-- Filtros para la tabla `artistas_canciones`
+--
+ALTER TABLE `artistas_canciones`
+  ADD CONSTRAINT `FK_ArtistasCanciones` FOREIGN KEY (`idArtistas`) REFERENCES `artistas` (`idArtistas`),
+  ADD CONSTRAINT `FK_CancionesArtistas` FOREIGN KEY (`idCanciones`) REFERENCES `canciones` (`idCanciones`);
+
+--
+-- Filtros para la tabla `canciones`
+--
+ALTER TABLE `canciones`
+  ADD CONSTRAINT `FK_ArtistaOriginal` FOREIGN KEY (`artistaOriginal`) REFERENCES `artistas` (`idArtistas`),
+  ADD CONSTRAINT `FK_Generos` FOREIGN KEY (`genero`) REFERENCES `generos` (`idGeneros`);
 
 --
 -- Filtros para la tabla `canciones_albumes`
